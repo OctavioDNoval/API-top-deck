4c35dcd4852480a66429dd59b96f03019b39c4ca
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-08-25 17:57:58 -0300

    Commit inicial

---
+++ src/main/java/org/example/topdeckapi/TopDeckApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.topdeckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopDeckApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopDeckApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/topdeckapi/TopDeckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.topdeckapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopDeckApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
