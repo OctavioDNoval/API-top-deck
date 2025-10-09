662077413bc59c08ce5e5a898c2d4dd4ffcc89cd
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-10-08 23:26:03 -0300

    Terminamos de crear todas las entidades de la base de datos

--- src/main/java/org/example/topdeckapi/src/model/Producto.java
+++ src/main/java/org/example/topdeckapi/src/model/Producto.java
@@ -1,4 +1,34 @@
 package org.example.topdeckapi.src.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "producto")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private long productoId;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column (name = "precio")
+    private float precio;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
 }
--- src/main/java/org/example/topdeckapi/src/model/Usuario.java
+++ src/main/java/org/example/topdeckapi/src/model/Usuario.java
@@ -1,4 +1,17 @@
 package org.example.topdeckapi.src.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "usuario")
 public class Usuario {
+
 }
