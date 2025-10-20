717e2a524e3b64ef49bc22b66b24b7e08863be27
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-10-19 23:44:00 -0300

    Set session management to stateless in security config
    Updated SecurityConfig to configure session management as stateless using SessionCreationPolicy.STATELESS. Also refactored CSRF disabling to use AbstractHttpConfigurer::disable for improved clarity and consistency.

--- src/main/java/org/example/topdeckapi/src/Security/SecurityConfig.java
+++ src/main/java/org/example/topdeckapi/src/Security/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.example.topdeckapi.src.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +13,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeHttpRequests(auth->{auth
-                        .anyRequest().permitAll();
-                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth-> auth
+                        .anyRequest().permitAll())
+                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(basic-> {});
 
         return http.build();
