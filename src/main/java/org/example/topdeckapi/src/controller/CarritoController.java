9ab81a1047dfafcb943a2e217c5ac0e38f2bfc23
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-10-25 00:02:49 -0300

    Add endpoint to get carrito by user ID
    Introduced GET /carrito/public/usuario/{id} endpoint in CarritoController to retrieve a user's carrito. This enhances API functionality by allowing clients to fetch carrito data for a specific user.

--- src/main/java/org/example/topdeckapi/src/controller/CarritoController.java
+++ src/main/java/org/example/topdeckapi/src/controller/CarritoController.java
@@ -1,15 +1,25 @@
 package org.example.topdeckapi.src.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.topdeckapi.src.model.Carrito;
 import org.example.topdeckapi.src.service.IMPL.CarritoService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
+@RequestMapping("/carrito")
 public class CarritoController {
     private final CarritoService carritoService;
+
+    @GetMapping("/public/usuario/{id}")
+    public ResponseEntity<Carrito> getCarritoByUser(@PathVariable("id") Long idUsuario){
+        Carrito carrito = carritoService.obtenerCarritoPorUsuario(idUsuario);
+        return ResponseEntity.ok(carrito);
+    }
 
     
 }
