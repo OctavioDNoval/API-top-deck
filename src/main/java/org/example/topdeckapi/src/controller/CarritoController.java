ce7e6d3090fa17ecbdff37c218dca31cc82ace40
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-10-27 22:05:44 -0300

    Add endpoints for managing cart details
    Introduces new endpoints to retrieve, add, and delete cart item details, as well as to empty the cart. Also updates existing endpoint paths for consistency.

--- src/main/java/org/example/topdeckapi/src/controller/CarritoController.java
+++ src/main/java/org/example/topdeckapi/src/controller/CarritoController.java
@@ -2,7 +2,9 @@ package org.example.topdeckapi.src.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.topdeckapi.src.model.Carrito;
+import org.example.topdeckapi.src.model.DetalleCarrito;
 import org.example.topdeckapi.src.service.IMPL.CarritoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +17,41 @@ import java.util.List;
 public class CarritoController {
     private final CarritoService carritoService;
 
-    @GetMapping("/public/usuario/{id}")
+    @GetMapping("/usuario/{id}")
     public ResponseEntity<Carrito> getCarritoByUser(@PathVariable("id") Long idUsuario){
         Carrito carrito = carritoService.obtenerCarritoPorUsuario(idUsuario);
         return ResponseEntity.ok(carrito);
     }
 
+    @GetMapping("/user/{idCarrito}/detalles")
+    public ResponseEntity<List<DetalleCarrito>> getDetalleCarrito(@PathVariable("idCarrito") Long idCarrito){
+        List<DetalleCarrito> detalles = carritoService.obtenerDetalleCarrito(idCarrito);
+        return ResponseEntity.ok(detalles);
+    }
+
+    @PostMapping("/user/{idCarrito}/save")
+    public ResponseEntity<DetalleCarrito> agregarDetalle (
+            @PathVariable("idCarrito") Long idCarrito,
+            @RequestParam Long idProducto,
+            @RequestParam int cantidad){
+        DetalleCarrito detalle = carritoService.agregarAlCarrito(idProducto, idCarrito, cantidad);
+        return ResponseEntity.status(HttpStatus.CREATED).body(detalle);
+    }
+
+    @DeleteMapping("/user/detalle/{idDetalle}")
+    public ResponseEntity<Void> eliminarProducto(@PathVariable("idDetalle") Long idDetalle){
+        boolean isDeleted = carritoService.deleteProducto(idDetalle);
+        if(isDeleted){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/user/{idCarrito}/empty")
+    public ResponseEntity<Void> vaciarCarrito(@PathVariable("idCarrito") Long idCarrito){
+        carritoService.borrarCarrito(idCarrito);
+        return ResponseEntity.noContent().build();
+    }
     
 }
